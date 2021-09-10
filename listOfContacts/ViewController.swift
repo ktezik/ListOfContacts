251497f8a1d2d90393c5621b1b3e2fe3cc0e5599
Ivan Grishin <ktezik@yandex.ru>
2021-09-10 17:48:39 +0300

    Initial Commit

---
+++ listOfContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  listOfContacts
+//
+//  Created by Иван Гришин on 10.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
